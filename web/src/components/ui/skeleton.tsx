7c1dc504b6f734912ba3fd25fca74793ce2e5799
Muhammad Rizqy Fadlilah <108173923+kirizqy@users.noreply.github.com>
2025-10-18 12:55:33 +0700

    Delete web directory

--- web/src/components/ui/skeleton.tsx
+++
@@ -1,6 +0,0 @@
-import * as React from "react";
-import { cn } from "./cn";
-
-export function Skeleton({ className }: { className?: string }) {
-  return <div className={cn("skel rounded-md", className)} />;
-}
